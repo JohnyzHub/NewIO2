d44b6152a2d7d6c87e098a49a82d2b53bc08eb40
johnyshaik <jbshaik82@gmail.com>
2016-08-28 09:24:55 -0400

    BufferExample with file altering file permissions

---
+++ src/com/newio/examples/channel/BufferExample.java
@@ -0,0 +1,75 @@
+package com.newio.examples.channel;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class BufferExample {
+
+	public static void main(String[] args) {
+		BufferExample bufferExample = new BufferExample();
+		try {
+			bufferExample.write();
+			bufferExample.read();
+			bufferExample.verifyPermissions();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void write() throws IOException {
+		Path path = Paths.get("ByteBufferExample.txt");
+		File file = path.toFile();
+		if (!file.canWrite()) {
+			System.out.println("File - " + file.getName() + " is READ-ONLY");
+			file.setWritable(true);
+			System.out.println("File - " + file.getName() + " permission changed to WRITABLE");
+		}
+
+		byte[] inputBytes = new String("This is a sample text for testing ByteBuffer.").getBytes();
+		ByteBuffer byteBuffer = ByteBuffer.wrap(inputBytes);
+
+		try (FileChannel channel = FileChannel.open(path, StandardOpenOption.CREATE, StandardOpenOption.WRITE);) {
+			channel.write(byteBuffer);
+		}
+	}
+
+	private void read() throws IOException {
+		Path path = Paths.get("ByteBufferExample.txt");
+		ByteBuffer byteBuffer = ByteBuffer.allocate(5);
+		try (FileChannel channel = FileChannel.open(path)) {
+			int bytesRead = channel.read(byteBuffer);
+			while (bytesRead > 0) {
+				// No need to flip buffer if reading all bytes at once as an
+				// array.
+				System.out.print(new String(byteBuffer.array()));
+				// Flip buffer if reading one byte each at a time, as below
+				// byteBuffer.flip();
+				// while (byteBuffer.hasRemaining()) {
+				// System.out.print((char) byteBuffer.get());
+				// }
+				byteBuffer.clear();
+				bytesRead = channel.read(byteBuffer);
+			}
+		}
+	}
+
+	private void verifyPermissions() throws IOException {
+		Path path = Paths.get("ByteBufferExample.txt");
+		File file = path.toFile();
+		boolean isWritable = Files.isWritable(path);
+		System.out.println("\nIs file - " + path.getFileName() + " ReadOnly - " + !file.canWrite());
+		if (isWritable) {
+			file.setReadOnly();
+			System.out.println("File - " + path.getFileName() + " permission changed to READ-ONLY? "
+					+ String.valueOf(!file.canWrite()));
+		}
+	}
+
+}
