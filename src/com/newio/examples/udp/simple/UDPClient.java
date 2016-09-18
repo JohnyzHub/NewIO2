573f31091d8a8f4143b2f3a103fa420318f00a51
johnyshaik <jbshaik82@gmail.com>
2016-09-18 11:57:17 -0400

    UDP Simple client-server example

---
+++ src/com/newio/examples/udp/simple/UDPClient.java
@@ -0,0 +1,46 @@
+package com.newio.examples.udp.simple;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class UDPClient {
+
+	public UDPClient() {
+		System.out.println("Client Started");
+		try (DatagramSocket client = new DatagramSocket(); Scanner scanner = new Scanner(System.in);) {
+			DatagramPacket packet = null;
+			String userMessage = null;
+			String messageReceived = null;
+			byte[] bytesSent = null;
+			byte[] bytesReceived = null;
+			InetAddress address = InetAddress.getByName("localhost");
+			while (true) {
+				System.out.println("\nEnter a message: ");
+				userMessage = scanner.nextLine();
+				if (userMessage.equalsIgnoreCase("EXIT") || userMessage.equalsIgnoreCase("QUIT")) {
+					break;
+				}
+				bytesSent = userMessage.getBytes();
+				packet = new DatagramPacket(bytesSent, bytesSent.length, address, 9003);
+				client.send(packet);
+				bytesReceived = new byte[1024];
+				packet = new DatagramPacket(bytesReceived, bytesReceived.length);
+				client.receive(packet);
+				messageReceived = new String(packet.getData());
+				System.out.println("Echo from server : " + messageReceived + " : " + packet.getAddress());
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Client Terminated");
+	}
+
+	public static void main(String[] args) {
+		new UDPClient();
+	}
+
+}
---
+++ src/com/newio/examples/udp/simple/UDPServer.java
@@ -0,0 +1,34 @@
+package com.newio.examples.udp.simple;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPServer {
+
+	public UDPServer() {
+		System.out.println("Server Started");
+		byte[] bytesReceived = null;
+		DatagramPacket packet = null;
+		try (DatagramSocket serverSocket = new DatagramSocket(9003);) {
+			while (true) {
+				bytesReceived = new byte[1024];
+				packet = new DatagramPacket(bytesReceived, bytesReceived.length);
+				serverSocket.receive(packet);
+				String messageReceived = new String(packet.getData());
+				System.out.println("Received : " + messageReceived + " : " + packet.getAddress());
+				bytesReceived = messageReceived.getBytes();
+				packet = new DatagramPacket(bytesReceived, bytesReceived.length, packet.getAddress(), packet.getPort());
+				serverSocket.send(packet);
+				System.out.println("Sent : " + messageReceived);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Server Terminated");
+	}
+
+	public static void main(String[] args) {
+		new UDPServer();
+	}
+}
